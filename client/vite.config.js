cb6a434e6dcb46761c34abe75dd1f190a672ce29
will <williamgeorge.barker@gmail.com>
2026-03-09 22:53:03 -0400

    build: Remove custom Rollup manual chunking configuration.

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,58 +9,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (!id.includes('node_modules')) {
-              return undefined
-            }
-
-            if (id.includes('@sentry/')) {
-              return 'sentry'
-            }
-
-            if (id.includes('@tanstack/react-query')) {
-              return 'query'
-            }
-
-            if (
-              id.includes('chart.js') ||
-              id.includes('react-chartjs-2')
-            ) {
-              return 'charts'
-            }
-
-            if (
-              id.includes('@fullcalendar/') ||
-              id.includes('react-big-calendar') ||
-              id.includes('/moment/') ||
-              id.includes('/date-fns/')
-            ) {
-              return 'calendar'
-            }
-
-            if (
-              id.includes('reactflow') ||
-              id.includes('react-d3-tree') ||
-              id.includes('/dagre/')
-            ) {
-              return 'fractal-graph'
-            }
-
-            if (
-              id.includes('react-router') ||
-              id.includes('@remix-run/router')
-            ) {
-              return 'router'
-            }
-
-            return 'vendor'
-          },
-        },
-      },
-    },
     server: {
       proxy: {
         '/api': {
